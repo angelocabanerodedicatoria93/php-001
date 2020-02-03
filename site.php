c2d7f8653f5b359fc2301e3546aafa81d03d8425
angelocabanerodedicatoria93 <angelodedicatoria93@gmail.com>
2020-02-03 17:25:09 +0800

    variables

--- site.php
+++ site.php
@@ -8,10 +8,34 @@
 
 <body>
   <?php
-  echo "<p>this is my site</p>";
-  echo "<h1>mike's site</h1>";
-  echo "<hr>";
-  // echo "<p>this is my site</p>";
+
+  // echo "there once was a man named george <br>";
+  // echo "he was 70 years old <br>";
+  // echo "he really liked the name george <br>";
+  // echo "but didn't like being 70 <br>";
+
+  // #####################################
+
+  // echo "there once was a man named john <br>";
+  // echo "he was 35 years old <br>";
+  // echo "he really liked the name john <br>";
+  // echo "but didn't like being 35 <br>";
+
+  // #####################################
+  // $characterName = "john";
+  // $characterAge = 35;
+
+  $characterName = "tom";
+  $characterAge = 80;
+
+  echo "there once was a man named $characterName <br>";
+  echo "he was $characterAge years old <br>";
+
+  $characterName = "mike";
+
+  echo "he really liked the name $characterName <br>";
+  echo "but didn't like being $characterAge <br>";
+
   ?>
 </body>
 
