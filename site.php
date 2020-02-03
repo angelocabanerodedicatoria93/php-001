4743ce7bf8b7af8b3a504d11eb4c3236e92ea75a
angelocabanerodedicatoria93 <angelodedicatoria93@gmail.com>
2020-02-03 20:06:09 +0800

    building a mad libs game

---
+++ site.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="UTF-8">
+  <title>Document</title>
+</head>
+
+<body>
+  <form action="site.php" method="get">
+    Color:
+    <input type="text" name="color"><br>
+    Plural Noun:
+    <input type="text" name="pluralNoun">
+    Celebrity:
+    <input type="text" name="celebrity">
+    <br>
+    <input type="submit">
+  </form>
+  <br><br>
+
+  <?php
+  $color = $_GET["color"];
+  $pluralNoun = $_GET["pluralNoun"];
+  $celebrity = $_GET["celebrity"];
+
+  echo "Roses are $color <br>";
+  echo "$pluralNoun are blue <br>";
+  echo "I love $celebrity <br>";
+  ?>
+
+</body>
+
+</html>
