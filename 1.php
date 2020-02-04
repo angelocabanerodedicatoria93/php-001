6784596763bf11aaa69c6deaad534aa3f2987ad0
angelocabanerodedicatoria93 <angelodedicatoria93@gmail.com>
2020-02-04 18:52:06 +0800

    brb 1

---
+++ 1.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+</head>
+<body>
+
+  <?php
+  /*
+    // $isMale = true;
+    $isMale = false;
+    if ($isMale) {
+      echo "you are male";
+    }
+  */
+
+    $isMale = false;
+    if ($isMale) {
+      echo "you are male";
+    } else {
+      echo "you are not male";
+    }
+
+  ?>
+</body>
+</html>
