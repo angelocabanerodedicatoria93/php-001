9ae102748c546cbd972c22ffd42315bc88ad24f4
angelocabanerodedicatoria93 <angelodedicatoria93@gmail.com>
2020-02-04 16:45:11 +0800

    return statements

---
+++ 1.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+</head>
+<body>
+
+  <?php
+  //  2^3 -> 2 * 2 * 2
+
+  /*
+    function cube($num) {
+      return $num * $num * $num;
+      echo "hello";
+    }
+
+    // $cubeResult = cube(4);
+    // echo $cubeResult;
+
+    echo cube(4);
+  */
+
+    function cube($num) {
+      echo "hello";
+      // return $num * $num * $num;
+      return;
+    }
+
+    echo cube(4);
+
+  ?>
+</body>
+</html>
