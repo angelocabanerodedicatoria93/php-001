cfe18ec36b50641c48c308a11568d9f3dc2b463e
angelocabanerodedicatoria93 <angelodedicatoria93@gmail.com>
2020-02-05 19:20:32 +0800

    include php

---
+++ site.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+</head>
+<body>
+
+  <?php
+  /*
+    $title = "My First Post";
+    $author = "Mike";
+    $wordCount = 400;
+
+    include "article-header.php"
+  */
+
+    include "useful-tools.php";
+
+    sayHi("mike");
+    echo $feetInMile;
+  ?>
+
+</body>
+</html>
