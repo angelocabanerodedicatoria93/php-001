de64071e60b052736543ca113183f756ccaec3f9
angelocabanerodedicatoria93 <angelodedicatoria93@gmail.com>
2020-02-05 17:16:56 +0800

    while loops

---
+++ 1.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+</head>
+<body>
+
+  <?php
+  /*
+    $index = 1;
+
+    while ($index <= 5) {
+      echo "$index <br>";
+      // $index++;
+
+      $index++;
+    }
+    */
+
+    $index = 6;
+
+    do {
+      echo "$index <br>";
+      $index++;
+    } while ($index <= 5);
+
+  ?>
+</body>
+</html>
