4e8890166266982e91e210b7ceb82722181e0f49
angelocabanerodedicatoria93 <angelodedicatoria93@gmail.com>
2020-02-06 17:14:42 +0800

    inheritance

---
+++ 1.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+</head>
+<body>
+
+  <?php
+
+    class Chef {
+
+      function makeChicken() {
+        echo "The chef makes chicken <br>";
+      }
+
+      function makeSalad() {
+        echo "The chef makes salad <br>";
+      }
+
+      function makeSpecialDish() {
+        echo "The chef makes bbq ribs <br>";
+      }
+    }
+
+    class ItalianChef extends Chef {
+      function makePasta() {
+        echo "The chef makes pasta";
+      }
+    }
+
+    $chef = new Chef();
+    // $chef->makeChicken();
+    // $chef->makePasta();
+    $chef->makeChicken();
+
+    $italianChef = new ItalianChef();
+    // $italianChef->makeChicken();
+    $italianChef->makePasta();
+  ?>
+
+</body>
+</html>
